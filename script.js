c99728e44608d7212161e063c3fb2bca50e958a7
bigjake-droid <manningjake2018@gmail.com>
2026-05-28 07:52:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,21 @@
-// WAIT FOR PAGE LOAD
-
 window.onload = () => {
 
-  // SPLASH SCREEN TIMER
+setTimeout(() => {
 
-  setTimeout(() => {
+document
+.getElementById("splashScreen")
+.classList.add("hidden");
 
-    // HIDE SPLASH
+document
+.getElementById("initiateScreen")
+.classList.remove("hidden");
 
-    document
-      .getElementById("splashScreen")
-      .classList.add("hidden");
-
-    // SHOW INITIATE SCREEN
-
-    document
-      .getElementById("initiateScreen")
-      .classList.remove("hidden");
-
-  }, 5000);
+}, 5000);
 
 };
 
-// ENTER APP
-
 function enterApp(type){
 
-  localStorage.setItem(
-    "caseforgeMode",
-    type
-  );
-
-  alert(
-    "Entering " + type + " Mode"
-  );
-
-  // FUTURE:
-  // window.location.href = "dashboard.html";
+alert("Entering " + type);
 
 }
